5042676df4d421b46262bd88577f12872019871c
kayon-assefa <kayonkau@gmail.com>
2026-05-03 18:01:39 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { Routes,Route } from "react-router-dom"
-import Register from "./pages/auth/Register"
-import Login from "./pages/auth/Login"
-import Dashboard from "./pages/Dashboard"
-import Donation from "./pages/Donation"
-import Withdraw from "./pages/Withdraw"
-import Settings from "./pages/Settings"
-import Livesub from "./pages/Livesub"
-function App(){
+import { Routes, Route } from "react-router-dom";
 
-return(
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/Dashboard";
+import Donation from "./pages/Donation";
+import Withdraw from "./pages/Withdraw";
+import Settings from "./pages/Settings";
+import Livesub from "./pages/Livesub";
 
-<Routes>
+import Home from "./pages/Home";     // ← New Landing Page
 
-<Route path="/register" element={<Register/>}/>
-<Route path="/login" element={<Login/>}/>
-<Route path="/dashboard" element={<Dashboard/>}/>
-<Route path="/donation" element={<Donation/>}/>
-<Route path="/settings" element={<Settings/>}/>
-<Route path="/withdraw" element={<Withdraw/>}/>
-<Route path="/livesub" element={<Livesub/>}/>
-</Routes>
-
-)
-
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />                    {/* Landing Page */}
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/donation" element={<Donation />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/withdraw" element={<Withdraw />} />
+      <Route path="/livesub" element={<Livesub />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
